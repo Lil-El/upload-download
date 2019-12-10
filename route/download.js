a1765d7b66dcd68c6d7cedd14c7cf21f1c7858ae
Lil-El <yxd99324@qq.com>
2019-12-10 14:02:15 +0800

    download demo

--- route/download.js
+++ route/download.js
@@ -1,6 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs")
+const path = require("path")
 
-router.get("/");
-
+router.get("/query",(req,res)=>{
+  let query = req.query
+  console.log(query)
+  let assetsPath = path.resolve(__dirname,`../assets/${query.file}/${query.type}`)
+  let isExists = fs.existsSync(assetsPath)
+  res.setHeader("Content-Type","text/plain;charset=utf-8")
+  if(!isExists){
+    res.end(JSON.stringify({
+      err:`您要找的 '${query.file}' 资源不存在`
+    }))
+  }
+  let statObj = fs.statSync(path.join(assetsPath,'music.mp4'))
+  res.end(JSON.stringify({
+      filename:`${query.file}`,
+      size:statObj.size
+  }))
+});
+router.get("/down",(req,res)=>{
+  let query = req.query
+  let assetsPath = path.resolve(__dirname,`../assets/${query.file}/${query.type}/music.mp4`)
+  var stats = fs.statSync(assetsPath); 
+  res.set({
+  'Content-Type': 'application/octet-stream',
+  'Content-Disposition': `attachment; filename=${query.file}.mp4`,
+  'Content-Length': stats.size
+  })
+  fs.createReadStream(assetsPath).pipe(res);
+});
 module.exports = router;
